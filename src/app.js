8bf3abbfa4e1c48bf76b1d12534697670a44d65b
Roberto <robercantalejo@gmail.com>
2025-01-24 21:20:46 +0000

    Hago el HTML, el CSS y randomizo los palos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/poker-hand.ico";
+
+let upperSuit = document.querySelector("#upper-suit");
+let lowerSuit = document.querySelector("#lower-suit");
+let suits = [upperSuit, lowerSuit];
+let randomNumber = document.querySelector("#random-number");
+function randomSuitNumber() {
+  return Math.floor(Math.random() * 4);
+}
+let suitValue = randomSuitNumber();
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  suits.forEach(suit => {
+    suit.innerText = suitValue;
+    if (suit.innerText == 0) {
+      suit.innerText = "♠";
+    } else if (suit.innerText == 1) {
+      suit.innerText = "♥";
+      suit.style.color = "red";
+    } else if (suit.innerText == 2) {
+      suit.innerText = "♣";
+    } else if (suit.innerText == 3) {
+      suit.innerText = "♦";
+      suit.style.color = "red";
+    }
+  });
 };
