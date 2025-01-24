6475e1b09d0a52a0a3f7cd7cbb64da37dc8e4d2f
Roberto <robercantalejo@gmail.com>
2025-01-24 23:52:50 +0000

    Añado un cursor personalizado

--- src/app.js
+++ src/app.js
@@ -11,10 +11,29 @@ function randomSuitNumber() {
   return Math.floor(Math.random() * 4);
 }
 let suitValue = randomSuitNumber();
+function randomCardValue() {
+  return Math.floor(Math.random() * 13);
+}
+let cardValues = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let chosenCardValue = cardValues[randomCardValue()];
 
 window.onload = function() {
   //write your code here
-
+  randomNumber.innerText = chosenCardValue;
   suits.forEach(suit => {
     suit.innerText = suitValue;
     if (suit.innerText == 0) {
