7c3ce4997af476b7fce10584b0934140baa498d2
Roberto <robercantalejo@gmail.com>
2025-01-25 12:29:36 +0000

    Arreglo varios bugs respecto al funcionamiento de los ejercicios extra

--- src/app.js
+++ src/app.js
@@ -50,18 +50,18 @@ function generateCard() {
     suit.innerText = suitValue;
     if (suit.innerText == 0) {
       suit.innerText = "♠";
+      suit.style.color = "black";
     } else if (suit.innerText == 1) {
       suit.innerText = "♥";
       suit.style.color = "red";
     } else if (suit.innerText == 2) {
       suit.innerText = "♣";
+      suit.style.color = "black";
     } else if (suit.innerText == 3) {
       suit.innerText = "♦";
       suit.style.color = "red";
     }
   });
-  //Tarea extra nº2: Pongo un temporizador de 10 segundos para que se vayan cambiando las cartas //
-  setTimeout(generateCard, 10000);
 }
 
 // Llamo a la función de generar carta al cargar la página //
@@ -76,6 +76,10 @@ let balatroButton = document.querySelector("#balatro-button");
 
 balatroButton.addEventListener("click", () => generateCard());
 
+//Tarea extra nº2: Pongo un temporizador de 10 segundos para que se vayan cambiando las cartas //
+
+setInterval(generateCard, 10000);
+
 // Esta parte asocia el ancho y alto de la carta dependiendo del número puesto //
 
 let projectInput = document.querySelector("#card-height");
@@ -85,6 +89,11 @@ let aspectRatioSuits = 0.18;
 let aspectRatioNumber = 0.28;
 
 function reSize() {
+  if (projectInput.value > 700) {
+    projectInput.value = 700;
+  } else if (projectInput.value < 100) {
+    projectInput.value = 100;
+  }
   let newHeight = projectInput.value;
   card.style.height = projectInput.value + "px";
   card.style.width = newHeight * aspectRatioWidth + "px";
